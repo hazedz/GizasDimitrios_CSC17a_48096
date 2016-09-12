2fa39ab92ee9a03b188c462c96e0478d912e633f
Dimitrios Gizas <dimitriosgizas@gmail.com>
2016-09-12 13:06:43 -0700

    Signed-off-by: Dimitrios Gizas <dimitriosgizas@gmail.com>

---
+++ Class/CPPTemplate/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: dimit
+ *
+ * Created on September 12, 2016, 1:01 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants 
+
+//Function Prototypes
+
+//Execution Begin Here!
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    //Input data
+    
+    //Process -> Map the input data to the output
+    
+    //Display/Output solution here
+
+    //Exit stage right!
+    return 0;
+}
+
